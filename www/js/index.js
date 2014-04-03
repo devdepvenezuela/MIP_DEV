1b3449a287d191d658292f3c7e499a1879c3dedb
devdepvenezuela <devdepvenezuela@gmail.com>
2014-04-03 01:44:01 -0430

    actualizacion
    actualizacion

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
-// Declaraci�n de variables globales
+// Declaraci?n de variables globales
 var myScroll, myScrollMenu, cuerpo, menuprincipal, wrapper, estado;
 
-// Guardamos en variables elementos para poder rescatarlos despu�s sin tener que volver a buscarlos
+// Guardamos en variables elementos para poder rescatarlos despu?s sin tener que volver a buscarlos
 cuerpo = document.getElementById("cuerpo"),
 menuprincipal = document.getElementById("menuprincipal"),
 wrapper = document.getElementById("wrapper");
@@ -14,14 +14,14 @@ var app = {
     	// Estado inicial mostrando capa cuerpo
     	estado="cuerpo";
     	
-    	// Creamos el elemento style, lo a�adimos al html y creamos la clase cssClass para aplicarsela al contenedor wrapper
+    	// Creamos el elemento style, lo a?adimos al html y creamos la clase cssClass para aplicarsela al contenedor wrapper
 	    var heightCuerpo=window.innerHeight-46;
 	    var style = document.createElement('style');
 	    style.type = 'text/css';
 	    style.innerHTML = '.cssClass { position:absolute; z-index:2; left:0; top:46px; width:100%; height: '+heightCuerpo+'px; overflow:auto;}';
 	    document.getElementsByTagName('head')[0].appendChild(style);
 	    
-	    // A�adimos las clases necesarias
+	    // A?adimos las clases necesarias
 		cuerpo.className = 'page center';
 		menuprincipal.className = 'page center';
 		wrapper.className = 'cssClass';
@@ -36,7 +36,7 @@ var app = {
 		xhReq.send(null);
 		document.getElementById("contenidoMenu").innerHTML=xhReq.responseText;
 		
-		// Creamos los 2 scroll mediante el plugin iscroll, uno para el men� principal y otro para el cuerpo
+		// Creamos los 2 scroll mediante el plugin iscroll, uno para el men? principal y otro para el cuerpo
 		myScroll = new iScroll('wrapper', { hideScrollbar: true });
 		myScrollMenu = new iScroll('wrapperMenu', { hideScrollbar: true });
 	
@@ -48,13 +48,13 @@ var app = {
     },
 
     onDeviceReady: function() {
-    	// Ejecutamos la funci�n FastClick, que es la que nos elimina esos 300ms de espera al hacer click
+    	// Ejecutamos la funci?n FastClick, que es la que nos elimina esos 300ms de espera al hacer click
     	new FastClick(document.body);
     },
     
 };
 
-// Funci�n para a�adir clases css a elementos
+// Funci?n para a?adir clases css a elementos
 function addClass( classname, element ) {
     var cn = element.className;
     if( cn.indexOf( classname ) != -1 ) {
@@ -66,7 +66,7 @@ function addClass( classname, element ) {
     element.className = cn+classname;
 }
 
-// Funci�n para eliminar clases css a elementos
+// Funci?n para eliminar clases css a elementos
 function removeClass( classname, element ) {
     var cn = element.className;
     var rxp = new RegExp( "\\s?\\b"+classname+"\\b", "g" );
@@ -76,7 +76,7 @@ function removeClass( classname, element ) {
 
 function menu(opcion){
 	
-	// Si pulsamos en el bot�n de "menu" entramos en el if
+	// Si pulsamos en el bot?n de "menu" entramos en el if
 	if(opcion=="menu"){
 		if(estado=="cuerpo"){
 			cuerpo.className = 'page transition right';
@@ -85,26 +85,26 @@ function menu(opcion){
 			cuerpo.className = 'page transition center';
 			estado="cuerpo";	
 		}
-	// Si pulsamos un bot�n del menu principal entramos en el else
+	// Si pulsamos un bot?n del menu principal entramos en el else
 	}else{
 		
-		// A�adimos la clase al li presionado
+		// A?adimos la clase al li presionado
 		addClass('li-menu-activo' , document.getElementById("ulMenu").getElementsByTagName("li")[opcion]);
 		
-		// Recogemos mediante ajax el contenido del html seg�n la opci�n clickeada en el menu
+		// Recogemos mediante ajax el contenido del html seg?n la opci?n clickeada en el menu
 		xhReq.open("GET", "opciones/opcion"+opcion+".html", false);
 		xhReq.send(null);
 		document.getElementById("contenidoCuerpo").innerHTML=xhReq.responseText;
 		
-		// Refrescamos el elemento iscroll seg�n el contenido ya a�adido mediante ajax, y hacemos que se desplace al top
+		// Refrescamos el elemento iscroll seg?n el contenido ya a?adido mediante ajax, y hacemos que se desplace al top
 		myScroll.refresh();
 		myScroll.scrollTo(0,0);
 		
-		// A�adimos las clases necesarias para que la capa cuerpo se mueva al centro de nuestra app y muestre el contenido
+		// A?adimos las clases necesarias para que la capa cuerpo se mueva al centro de nuestra app y muestre el contenido
 		cuerpo.className = 'page transition center';
 		estado="cuerpo";
 		
-		// Quitamos la clase a�adida al li que hemos presionado
+		// Quitamos la clase a?adida al li que hemos presionado
 		setTimeout(function() {
 			removeClass('li-menu-activo' , document.getElementById("ulMenu").getElementsByTagName("li")[opcion]);
 		}, 300);
