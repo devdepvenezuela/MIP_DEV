20808b5a5c26a9c103574fb18d52d8b785bf5bd7
devdepvenezuela <devdepvenezuela@gmail.com>
2014-04-15 23:44:23 -0430

    combo

--- www/js/index.js
+++ www/js/index.js
@@ -136,12 +136,6 @@ function menu(opcion){
 			if (pais != null)
 				{docpais.value = pais; }
 			}
-			
-			if(opcion == '1')
-		{
-			var btnreg = document.getElementById("btnregister");
-			btnreg.disabled = true;
-		}
 		
 		// A?adimos las clases necesarias para que la capa cuerpo se mueva al centro de nuestra app y muestre el contenido
 		cuerpo.className = 'page transition center';
