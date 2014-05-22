aa4d57d820efc799c1e02b87c41caee0a230c5aa
devdepvenezuela <devdepvenezuela@gmail.com>
2014-05-22 15:00:34 -0430

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -29,7 +29,7 @@ var app = {
 		// Leemos por ajax el archivos opcion1.html de la carpeta opciones
 		xhReq.open("GET", "opciones/opcion0.html", false);
 		xhReq.send(null);
-		document.getElementById("contenidoCuerpo1").innerHTML=xhReq.responseText;
+		document.getElementById("contenidoCuerpo").innerHTML=xhReq.responseText;
 
 		// Leemos por ajax el archivos menu.html de la carpeta opciones
 		xhReq.open("GET", "opciones/menu.html", false);
@@ -70,8 +70,6 @@ myScroll = new iScroll('wrapper', {
             }
         }
     });
-	myScroll.destroy();
-	myScroll = null;
         this.bindEvents();
     },
 
@@ -126,7 +124,7 @@ function menu(opcion){
 		// Recogemos mediante ajax el contenido del html seg?n la opci?n clickeada en el menu
 		xhReq.open("GET", "opciones/opcion"+opcion+".html", false);
 		xhReq.send(null);
-		document.getElementById("contenidoCuerpo1").innerHTML=xhReq.responseText;
+		document.getElementById("contenidoCuerpo").innerHTML=xhReq.responseText;
 		
 		// Refrescamos el elemento iscroll seg?n el contenido ya a?adido mediante ajax, y hacemos que se desplace al top
 		/*myScroll.refresh();
